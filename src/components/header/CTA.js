03c21a4147bb6b3510269c87943a81a390a12191
studentjsw <hema47173@gmail.com>
2024-04-01 10:17:37 +0530

    first commit

---
+++ src/components/header/CTA.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import CV from '../../assests/jeeva5.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+      {/* <a href={CV} download className='btn'>Resume</a> */}
+      {/* <a href='https://drive.google.com/file/d/1IUeCngMxk27QdMR4fWJ2nSDmu8Bt9Z8J/view?usp=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a> */}
+      <a href='https://drive.google.com/file/d/1W-hkHg5imur8Xk8V_3kZLkf_h_Djf3CC/view?usp=drive_link=sharing' target="_blank" rel='noreferrer' className='btn'>Resume</a>
+      <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
+    </div>
+  )
+}
+
+export default CTA
